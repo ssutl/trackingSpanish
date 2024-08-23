c838122c79fa365088511830dbb1a21e2daab49c
Shay Campbell <shayc.campbell@gmail.com>
2024-08-24 00:22:20 +0100

    Create account on signin

--- out.js
+++ out.js
@@ -1,39 +1,38 @@
-const fs = require('fs');
-const glob = require('glob');
-const path = require('path');
+const fs = require("fs");
+const glob = require("glob");
+const path = require("path");
 
 // Copy background.js from public directory to out directory
-const backgroundSource = path.join(__dirname, 'public', 'background.js');
-const backgroundDestination = path.join(__dirname, 'out', 'background.js');
+const backgroundSource = path.join(__dirname, "public", "background.js");
+const backgroundDestination = path.join(__dirname, "out", "background.js");
 fs.copyFileSync(backgroundSource, backgroundDestination);
 
 // Copy offscreen.html from public directory to out directory
-const offscreenHtmlSource = path.join(__dirname, 'public', 'offscreen.html');
-const offscreenHtmlDestination = path.join(__dirname, 'out', 'offscreen.html');
+const offscreenHtmlSource = path.join(__dirname, "public", "offscreen.html");
+const offscreenHtmlDestination = path.join(__dirname, "out", "offscreen.html");
 fs.copyFileSync(offscreenHtmlSource, offscreenHtmlDestination);
 
 // Copy offscreen.js from public directory to out directory
-const offscreenJsSource = path.join(__dirname, 'public', 'offscreen.js');
-const offscreenJsDestination = path.join(__dirname, 'out', 'offscreen.js');
+const offscreenJsSource = path.join(__dirname, "public", "offscreen.js");
+const offscreenJsDestination = path.join(__dirname, "out", "offscreen.js");
 fs.copyFileSync(offscreenJsSource, offscreenJsDestination);
 
-
 // Modify HTML files
-const files = glob.sync('out/**/*.html');
+const files = glob.sync("out/**/*.html");
 files.forEach((file) => {
-  const content = fs.readFileSync(file, 'utf-8');
-  const modifiedContent = content.replace(/\/_next/g, './next');
-  fs.writeFileSync(file, modifiedContent, 'utf-8');
+  const content = fs.readFileSync(file, "utf-8");
+  const modifiedContent = content.replace(/\/_next/g, "./next");
+  fs.writeFileSync(file, modifiedContent, "utf-8");
 });
 
 // Rename _next directory to next
-const sourcePath = 'out/_next';
-const destinationPath = 'out/next';
+const sourcePath = "out/_next";
+const destinationPath = "out/next";
 
 fs.rename(sourcePath, destinationPath, (err) => {
   if (err) {
-    console.error('Error renaming directory:', err);
+    console.error("Error renaming directory:", err);
   } else {
-    console.log('Directory renamed successfully');
+    console.log("Directory renamed successfully");
   }
 });
